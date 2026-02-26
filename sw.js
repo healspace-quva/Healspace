cb7589276d5a086fa08dcbd6654f7640b4a1a98f
healspace-quva <thilakjeevesh@gmail.com>
2026-02-26 19:55:47 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,15 @@
-const CACHE_NAME = 'healspace-cache-v1';
-const urlsToCache = [ '/', '/login.html', '/profile.html', '/manifest.json', '/logo-512.png' ];
-
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+self.addEventListener('activate', (event) => {
+    event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        fetch(event.request).catch(() => {
+            return new Response("Offline mode active.");
+        })
+    );
 });
